8977e99e3721620526e88c0add7798f3c74e5a2e
n0tpurplx <practicalcharacter@gmail.com>
2026-09-09 23:47:06 +0200

    Update author and theme in Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -2,17 +2,19 @@
 
 project = "PurpleCli"
 copyright = "2026, PurpleCli"
-author = "Notpurplx"
+author = "PurpleCli"
 
-# Extensions
 extensions = []
 
-# Templates
 templates_path = ["_templates"]
-
-# Exclude patterns
 exclude_patterns = []
 
-# HTML output
-html_theme = "alabaster"
+# Theme
+html_theme = "furo"
+
+# Branding
+html_title = "PurpleCli Documentation"
+
+# Custom CSS
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
