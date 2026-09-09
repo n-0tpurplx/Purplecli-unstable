3c7267f21b1903b64eb23e1ff77ed2a1c77f2828
n0tpurplx <practicalcharacter@gmail.com>
2026-09-09 23:51:00 +0200

    Update documentation configuration for PurpleCli

--- docs/conf.py
+++ docs/conf.py
@@ -8,4 +8,8 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 html_theme = "alabaster"
+
 html_title = "PurpleCli Documentation"
+
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
